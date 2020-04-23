3ad43696ebd8f15c57510bb3efe76150f72c2a7d
Andrew Lamansky <andrewlamansky@gmail.com>
2020-04-23 12:48:53 -0400

    modify cookie logic to work in both dev and prod - remove non-essential lines in default.conf nginx config - edit cors config obj in index.js - conditionally set sameSite on JWT cookie based on NODE_ENV

--- src/util/sendJWT.js
+++ src/util/sendJWT.js
@@ -1,11 +1,15 @@
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = function sendJWT(ctx, user) {
   const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
 
+  // if env is dev, cookies will be sent with sameSite: "Lax", secure: false
+  // if env is prod, cookies will be sent with sameSite: "None", secure: true
   ctx.response.cookie("token", token, {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7 * 52,
-    sameSite: "lax"
+    sameSite: process.env.NODE_ENV == "dev" ? "Lax" : "None",
+    secure: process.env.NODE_ENV == "dev" ? false : true,
   });
 };
